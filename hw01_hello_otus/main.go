322e9628b10a8cee240d7d9942b6f945840af83d
s0meuserhere <@.>
2026-05-29 19:25:53 +0300

    HW01 - Complete

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	sourceStr := "Hello, OTUS!"
+	reversedStr := Reverse(sourceStr)
+	fmt.Print(reversedStr)
+}
+
+// Reverse - выполняет переворот строки.
+func Reverse(s string) string {
+	return reverse.String(s)
 }
